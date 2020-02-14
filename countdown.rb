760b94f38495c5054a3fdc3edf1e34de1f7b88d5
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-14 21:00:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,12 +9,12 @@ def countdown(counter)
     "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  y = 10 
+def countdown_with_sleep(counter)
+  
   new_year = "HAPPY NEW YEAR"
-  while y > 0 
+  while counter > 0 
     puts "#{y} SECOND(S)!"
-    y -= 1 
+    counter -= 1 
     sleep 1 
   end
     new_year
